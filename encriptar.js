a5543083aa9df0a986388cf4b1f5cd8bf6f5ff22
sirsblad <130020448+sirsblad@users.noreply.github.com>
2023-04-26 16:05:07 -0600

    encritador alura
    por parte de josue daniel moreno garcia

---
+++ encriptar.js
@@ -0,0 +1,43 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (document.getElementById("texto").value.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      document.getElementById("titulo-mensaje").textContent =
+        "Texto encriptado con éxito";
+      document.getElementById("parrafo").textContent = "";
+      
+    } else {
+     
+      swal("Ooops!", "Debes ingresar un texto!", "warning");
+    }
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+  
+    if (document.getElementById("texto").value.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      document.getElementById("titulo-mensaje").textContent =
+        "Texto desencriptado con éxito";
+      document.getElementById("parrafo").textContent = "";
+      
+    } else {
+      
+      swal("Ooops!", "Debes ingresar un texto!", "warning");
+    }
+  }
